576f498b305e84f5c77486b25338050110181e0a
Hitesh Kumar Sethi <147877147+Aaroecode@users.noreply.github.com>
2025-02-22 00:02:03 +0530

    frontend-upload

---
+++ workwise-frontend-main/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Import path module
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Add this alias
+    },
+  },
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+});
